4753d6816ac0c49cac2fdd794d44f88d21843dc0
M0svmv <medoosama822@gmail.com>
2026-05-03 01:38:09 +0300

    prof gets this sem courses only

--- backend/controllers/staff/lecturer/lecturer.controller.js
+++ backend/controllers/staff/lecturer/lecturer.controller.js
@@ -13,12 +13,14 @@ const { Cursor } = require("mongoose");
 //get lecturer courses
 exports.getLecturerCourses = async (req, res) => {
   try {
+    const semester = await Semester.findOne({ isCurrent: true });
     const lecturer = await Staff.findById(req.user._id).select("-password");
     if (!lecturer) {
       return res.status(404).json({ message: "Lecturer not found" });
     }
     const courses = await CourseOffering.find({
       instructorId: lecturer._id,
+      semesterId: semester._id,
     }).populate("courseId");
 
     res.status(200).json(courses);
