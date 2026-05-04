c989395f9d3b4d4a234a5c86fc26ccf3168baf5f
M0svmv <medoosama822@gmail.com>
2026-05-04 23:53:12 +0300

    fixing control auth role

--- backend/routes/controlMember.route.js
+++ backend/routes/controlMember.route.js
@@ -6,19 +6,19 @@ const authMiddleware = require('../middlewares/auth.middleware');
 const roleCheckMiddleware = require('../middlewares/role.middleware');
 
 // Get all courses with enrolled students
-router.get('/courses', authMiddleware, roleCheckMiddleware(['control-member']), controlMemberController.getAllCourses);
+router.get('/courses', authMiddleware, roleCheckMiddleware('control-member'), controlMemberController.getAllCourses);
 
 // Get all students in a course
-router.get('/courses/:id/students', authMiddleware, roleCheckMiddleware(['control-member']), controlMemberController.getCourseData);
+router.get('/courses/:id/students', authMiddleware, roleCheckMiddleware('control-member'), controlMemberController.getCourseData);
 
 // Assign final grades to students in a course
-router.put('/courses/:id/assign-final-grades', authMiddleware, roleCheckMiddleware(['control-member']), controlMemberController.assignCourseFinalGrades);
+router.put('/courses/:id/assign-final-grades', authMiddleware, roleCheckMiddleware('control-member'), controlMemberController.assignCourseFinalGrades);
 
 // approve final grades for a course
-router.put('/courses/:id/approve-final-grades', authMiddleware, roleCheckMiddleware(['control-member']), controlMemberController.approveFinalGrades);
+router.put('/courses/:id/approve-final-grades', authMiddleware, roleCheckMiddleware('control-member'), controlMemberController.approveFinalGrades);
 
 // Update final grades for students in a course
-router.put('/courses/:id/update-grades', authMiddleware, roleCheckMiddleware(['control-member']), controlMemberController.updateCourseGrades);
+router.put('/courses/:id/update-grades', authMiddleware, roleCheckMiddleware('control-member'), controlMemberController.updateCourseGrades);
 
 
 
